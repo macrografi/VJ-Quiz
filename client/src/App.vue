2aa67965258f73f2d16277e06537af7ad4c63744
cenk özkan <macrografi@gmail.com>
2019-07-20 15:20:39 +0300

    work quiz

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,21 @@
   <b-container class="bv-example-row">
     <b-row class="justify-content-md-center">
       <b-col md="10">
-        <h1 style="margin-top:20px; text-align:center;">{{verb}} / {{tense}} / {{meaning}}</h1>
-        <!-- <b-form-input v-model="answer" placeholder="Enter your name"></b-form-input>
-
-        <div style="margin-top:20px;">
-          <b-button variant="primary" @click="clickAnswer">Primary</b-button>
-          <b-button variant="secondary">Secondary</b-button>
-        </div>-->
-        
-        <div class="mt-2" style="display:none">{{myArray}}</div>
-        <b-button variant="primary" @click="clickAnswer">Primary</b-button>
+        <div v-if="isShow">
+          <h1 style="margin-top:20px; text-align:center;">{{verb}} / {{tense}} / {{meaning}}</h1>
+          <b-progress height="20px" :value="max * (count / length)" show-progress class="mb-2"></b-progress>
+          <br />
+          <b-form-input v-model="answer" placeholder="Enter your answer"></b-form-input>
+          <br />
+          <b-button variant="primary" @click="clickAnswer">Cevapla</b-button>
+        </div>
+        <div v-if="isEnd">
+          <h1>Test Bitti</h1>
+          <p>Puanınız</p>
+          <p>Doğru Cevap : 1</p>
+          <p>Yanlış Cevap : 1</p>
+        </div>
+        <b-button v-if="!isShow" variant="primary" @click="startQuiz">{{buttonText}}</b-button>
       </b-col>
     </b-row>
   </b-container>
@@ -22,15 +27,39 @@ import { mapGetters } from "vuex";
 export default {
   data() {
     return {
+      isShow: false,
+      isEnd: false,
       myArray: [],
+      answer: "",
       verb: "",
       tense: "",
       meaning: "",
-      randomFunct: function(val) {
-        let randomData = val[Math.floor(Math.random() * 10)];
+      length: 0,
+      max: 100,
+      count: -1,
+      buttonText: "teste başla",
+      randomize: function() {
+        let myData = this.myArray[0].verbs;
+        let length = myData.length;
+        this.length = length;
+        let randomData = myData[Math.floor(Math.random() * 10)];
         this.verb = randomData.verb;
         this.tense = randomData.tense;
         this.meaning = randomData.meaning;
+        this.count += 1;
+
+        if (this.count >= this.length) {
+          setTimeout(() => {
+            this.isEnd = true;
+            this.isShow = false;
+            this.buttonText = "tekrar";
+            this.count = -1;
+          }, 700);
+        } 
+        else {
+          this.isEnd = false;
+          this.isShow = true;
+        }
       }
     };
   },
@@ -39,14 +68,21 @@ export default {
     let myVerbs = this.$store.getters;
     this.myArray.push(myVerbs);
   },
-  beforeUpdate() {
-    let myData = this.myArray[0].verbs;
-    this.randomFunct(myData);
-  },
   methods: {
+    startQuiz() {
+      this.isShow = true;
+      this.randomize();
+    },
     clickAnswer() {
-      let myData = this.myArray[0].verbs;
-      this.randomFunct(myData);
+      let payload = {
+        answer: this.answer
+      };
+      if (payload.answer == this.meaning) {
+        console.log("true");
+      } else {
+        console.log("false");
+      }
+      this.randomize();
     }
   }
 };
